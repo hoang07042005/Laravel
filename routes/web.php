f9a3273be1dd1741a37e82a44b9cd61022a8d793
hoang07042005 <doviethoang07042005@gmail.com>
2024-01-19 18:37:55 +0700

    .....

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,40 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('home', 'App\Http\Controllers\HomeController@showWelcome');
+Route::get('about','App\Http\Controllers\AboutController@showDetails');
+Route::get('profile/{name}','App\Http\Controllers\ProfileController@showProfile');
+
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+//Route::get('about',function() {
+//    return 'About Content';
+//});
+
+Route::get('about/directions',function() {
+    return'Directions go here';
+});
+
+Route::any('submit-form',function() {
+    return 'Process Form';
+});
+
+Route::get('about/{theSubject}', 'App\Http\Controllers\AboutController@showSubject');
+
+Route::get('about/classes/{theSubject}', function ($theSubject) {
+    return " Content on $theSubject ";
+});
+
+Route::get('about/classes/{theArt}/{thePrince}',function ($theArt, $thePrince) {
+    return "The product: $theArt and $thePrince";
+});
+
+Route::get('where', function() {
+    return Redirect::route('direction');
 });
